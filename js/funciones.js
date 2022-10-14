a992acb4b101224af06ea863045d2482043da9b3
gaston borges <gbsm00@gmail.com>
2022-10-13 22:56:07 -0300

    first commit

--- js/funciones.js
+++ js/funciones.js
@@ -3,22 +3,23 @@ const lista = document.getElementById("lista")
 const listado = document.querySelector('#listado')
 const nombreIngresar = document.querySelector("#nombreingresado")
 const acordeIngresar = document.querySelector("#acordesingresados")
-const URL = `https://www.theaudiodb.com/api/v1/json/2/search.php?s=rhcp` 
+const URL = `https://6348b8dea59874146b0f966c.mockapi.io/api/artistas` 
 let info = []
 
 const rhcpFetch = () => {
     fetch(URL)
     .then((response) => response.json())
     .then (data => info = data)
+    
 }
 rhcpFetch();
 
 function mostrarNombresRHCP() {
            const li = document.createElement('li')
             li.innerHTML = `
-            <h3>${info.artists[0].strArtist}</h3>
-            <p>${info.artists[0].strGenre}</p>
-            <p>${info.artists[0].strBiographyEN}</p>
+            <h3>${info[0].Nombre}</h3>
+            <p>${info[0].Genero}</p>
+            <p>${info[0].Informacion}</p>
             `
             listado.append(li)
         }
